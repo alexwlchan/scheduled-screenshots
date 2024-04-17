8e9a39fc5b0a9f3c7d321b2c657f8e9951b3cbb7
Alex Chan <alex@alexwlchan.net>
2024-04-17 21:29:22 +0100

    Add a script for creating screenshots

---
+++ create_screenshots.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+mkdir -p screenshots
+
+today=$(date +"%Y-%m-%d")
+
+npx playwright screenshot https://alexwlchan.net "screenshots/alexwlchan.net.$today.png"
+git add "screenshots/alexwlchan.net.$today.png"
+
+git commit -m "Add screenshots for $today"
+git push origin main
