56396d079718de142fe4ce48b50f446f7d2321b8
Alex Chan <alex@alexwlchan.net>
2024-04-21 09:43:14 +0100

    Use and document the new variables

--- take_screenshot.sh
+++ take_screenshot.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
+# Take a screenshot of a website, save it to the `screenshots` folder,
+# and push it to GitHub.
+#
+# This script needs two environment variables:
+#
+#   - URL = the URL of the website that you want to screenshot
+#   - SCREENSHOT_NAME = the prefix of the names in the screenshots foler
+#
 
 set -o errexit
 set -o nounset
 
 mkdir -p screenshots
 
-DOMAIN="alexwlchan.net"
-SCREENSHOT_NAME="alexwlchan.net"
-
 today=$(date +"%Y-%m-%d")
 screenshot_path="screenshots/$SCREENSHOT_NAME.$today.png"
 
 if [[ ! -f "$screenshot_path" ]]
 then
-  npm install playwright-chromium
-
   npx playwright screenshot \
     --full-page \
     --wait-for-timeout 10000 \
-    "$DOMAIN_NAME" "$screenshot_path"
+    "$URL" "$screenshot_path"
   git add "$screenshot_path"
 
   git commit -m "Add screenshots for $today"
